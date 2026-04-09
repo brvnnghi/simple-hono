355108ca5f500665eaa1015e2d4b321633eaba61
Alex Nguyen <nghi@brandsvietnam.com>
2026-04-09 09:51:23 +0700

    App with Tailwind CSS (build-on-deploy) (fix)

--- src/components/layouts/SiteLayout.tsx
+++ src/components/layouts/SiteLayout.tsx
@@ -4,12 +4,14 @@ import { SiteNavigation } from '../navigation/SiteNavigation'
 type SiteLayoutProps = PropsWithChildren
 
 export const SiteLayout: FC<SiteLayoutProps> = ({ children }) => {
+  const cssVersion = Math.random().toString(36).slice(2)
+
   return (
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="/app.css" />
+        <link rel="stylesheet" href={`/app.css?v=${cssVersion}`} />
       </head>
       <body className="min-h-screen bg-slate-100 text-slate-800 antialiased">
         <SiteNavigation />
